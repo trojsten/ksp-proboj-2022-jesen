eaa557852774c2a34ae8b782466a39466b855784
Marcel Palaj <palaj.marcel@gmail.com>
2022-11-22 18:20:01 +0100

    dačo som urobil (break tiles)

--- game/turn/building.go
+++ game/turn/building.go
@@ -1,6 +1,9 @@
 package turn
 
-import "ksp.sk/proboj/73/game"
+import (
+	"ksp.sk/proboj/73/game"
+	"ksp.sk/proboj/73/game/inventory"
+)
 
 func (t Turn) SettleBuilding() {
 	// First, place new tiles
@@ -18,12 +21,25 @@ func (t Turn) SettleBuilding() {
 	// Then, break old tiles
 	for _, change := range t.TileChanges {
 		if change.To == game.Empty {
-			continue
+			tile := t.Game.World.Tiles[change.Where.Y][change.Where.X]
+			switch tile.Type() {
+			case game.Stone:
+				change.Lemur.AddItem(inventory.Stone, 1)
+			case game.Coal:
+				change.Lemur.AddItem(inventory.Coal, 1)
+			case game.Gold:
+				change.Lemur.AddItem(inventory.Gold, 1)
+			case game.Furnace:
+				change.Lemur.AddItem(inventory.Gold, 1)
+			case game.Chest:
+				change.Lemur.AddItem(inventory.Coal, 1)
+			case game.Tree:
+				change.Lemur.AddItem(inventory.Cocos, 1)
+			case game.Trap:
+				change.Lemur.AddItem(inventory.Stone, 1)
+			}
+			// TODO treba to oznacit ako Empty
+			// t.Game.World.Tiles[change.Where.Y][change.Where.X] = game.Empty
 		}
-
-		// TODO
-		// t.Game.World.Tiles[change.Where.Y][change.Where.X]
-
-		// TODO: AddItem material to lemur's inventory
 	}
 }
