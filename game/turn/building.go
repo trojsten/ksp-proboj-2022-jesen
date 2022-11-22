2eebe4480be4748f3b0fadea402b60f19cc72f2a
Marcel Palaj <palaj.marcel@gmail.com>
2022-11-22 18:32:35 +0100

    ofixoval som, čo som urobil (break a build)

--- game/turn/building.go
+++ game/turn/building.go
@@ -8,14 +8,21 @@ import (
 func (t Turn) SettleBuilding() {
 	// First, place new tiles
 	for _, change := range t.TileChanges {
-		if change.To == game.Empty {
-			continue
+		switch change.To {
+		case game.Furnace:
+			change.Lemur.RemoveItem(inventory.Gold, 1)
+			// t.Game.World.Tiles[change.Where.Y][change.Where.X] = game.Furnace
+		case game.Chest:
+			change.Lemur.RemoveItem(inventory.Coal, 1)
+			// t.Game.World.Tiles[change.Where.Y][change.Where.X] = game.Chest
+		case game.Tree:
+			change.Lemur.RemoveItem(inventory.Cocos, 1)
+			// t.Game.World.Tiles[change.Where.Y][change.Where.X] = game.Tree
+		case game.Trap:
+			change.Lemur.RemoveItem(inventory.Stone, 1)
+			// t.Game.World.Tiles[change.Where.Y][change.Where.X] = game.Trap
 		}
-
-		// TODO
-		// t.Game.World.Tiles[change.Where.Y][change.Where.X]
-
-		// TODO: RemoveItem material from lemur's inventory
+		// TODO treba budovu postavit
 	}
 
 	// Then, break old tiles
@@ -25,21 +32,15 @@ func (t Turn) SettleBuilding() {
 			switch tile.Type() {
 			case game.Stone:
 				change.Lemur.AddItem(inventory.Stone, 1)
+				// t.Game.World.Tiles[change.Where.Y][change.Where.X] = game.Empty
 			case game.Coal:
 				change.Lemur.AddItem(inventory.Coal, 1)
+				// t.Game.World.Tiles[change.Where.Y][change.Where.X] = game.Empty
 			case game.Gold:
 				change.Lemur.AddItem(inventory.Gold, 1)
-			case game.Furnace:
-				change.Lemur.AddItem(inventory.Gold, 1)
-			case game.Chest:
-				change.Lemur.AddItem(inventory.Coal, 1)
-			case game.Tree:
-				change.Lemur.AddItem(inventory.Cocos, 1)
-			case game.Trap:
-				change.Lemur.AddItem(inventory.Stone, 1)
+				// t.Game.World.Tiles[change.Where.Y][change.Where.X] = game.Empty
 			}
 			// TODO treba to oznacit ako Empty
-			// t.Game.World.Tiles[change.Where.Y][change.Where.X] = game.Empty
 		}
 	}
 }
