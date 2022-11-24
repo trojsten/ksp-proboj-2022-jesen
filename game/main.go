4671671df5f5da00afd77c8be25e083bc57ebace
Adam Zahradník <adam@zahradnik.xyz>
2022-11-24 14:44:15 +0100

    add game setup

--- game/main.go
+++ game/main.go
@@ -4,6 +4,23 @@ import "ksp.sk/proboj/73/libproboj"
 
 func New(r libproboj.Runner) Game {
 	g := Game{runner: r}
+
+	players, _ := r.ReadConfig()
+
+	for i, player := range players {
+		g.Players = append(g.Players, Player{
+			Idx:         i,
+			Name:        player,
+			Color:       "",
+			DisplayName: "",
+			Lemurs:      []Lemur{},
+		})
+
+		/*for i := 0; i < 5; i++ {
+			g.Players[i].Lemurs = append(g.Players[i].Lemurs, Lemur{})
+		}*/
+	}
+
 	return g
 }
 
---
+++ libproboj/setup.go
@@ -0,0 +1,17 @@
+package libproboj
+
+import (
+	"fmt"
+	"strings"
+)
+
+func (r Runner) ReadConfig() ([]string, string) {
+	line := r.readLine()
+	if line != "CONFIG" {
+		panic(fmt.Errorf("expected CONFIG, got %s", line))
+	}
+
+	players := strings.Split(r.readLine(), " ")
+	data := r.readLines()
+	return players, data
+}
