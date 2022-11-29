e681e19432d6e25d586984d4c7414b7095e37fa4
Adam Zahradník <adam@zahradnik.xyz>
2022-11-29 22:20:47 +0100

    kill lemurs standing in the dark

--- game/lemurs.go
+++ game/lemurs.go
@@ -15,6 +15,7 @@ type Lemur struct {
 	Stone       int
 	Gold        int
 	LanternTime int
+	TimeInDark  int
 }
 
 func (l *Lemur) AddItem(slot inventory.InventorySlot, quantity int) {
@@ -130,4 +131,20 @@ func SpawnLemur(p *Player, g *Game) error {
 		Tools:    [2]Tool{Pickaxe, NoTool},
 	})
 	return nil
+}
+
+func (l *Lemur) Tick(g *Game) {
+	if !l.Alive {
+		return
+	}
+
+	if g.World.Light[l.Position.Y][l.Position.X] <= 0 {
+		// The lemur is standing in the dark
+		l.TimeInDark++
+		if l.TimeInDark >= 4 {
+			l.Alive = false
+		}
+	} else {
+		l.TimeInDark = 0
+	}
 }
