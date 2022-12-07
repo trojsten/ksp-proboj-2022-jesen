8b787ec96a408830a30331e0b7e7bfc831328c9d
Matej Novota <matej.novota@gmail.com>
2022-12-07 16:04:31 +0100

    master: reformat files

--- templates/cpp/common.h
+++ templates/cpp/common.h
@@ -9,306 +9,224 @@
 #include <iterator>
 #include <sstream>
 
-
-namespace std {
-	template<class T>
-	ostream& operator<< (ostream& out, const vector<T>& v) {
-		ranges::copy(v, ostream_iterator<T>(out, " "));
-		out << '\n';
-		return out;
-	}
-}
-
-enum class CommandType: int {
-	NOOP,
-	STAB,
-	BONK,
-	BUILD,
-	BREAK,
-	DISCARD,
-	PUT,
-	TAKE,
-	CRAFT,
-	MOVE,
+enum class CommandType : int {
+    NOOP,
+    STAB,
+    BONK,
+    BUILD,
+    BREAK,
+    DISCARD,
+    PUT,
+    TAKE,
+    CRAFT,
+    MOVE,
 };
 
-std::ostream& operator<< (std::ostream& out, const CommandType& cmd);
-
-enum class InventorySlot: int {
-	LEMON,
-	STONE,
-	IRON,
-	TOOL1,
-	TOOL2,
+enum class InventorySlot : int {
+    LEMON,
+    STONE,
+    IRON,
+    TOOL1,
+    TOOL2,
 };
 
-std::ostream& operator<< (std::ostream& out, const InventorySlot& i);
-
-enum class Tool: int {
-	JUICER,
-	PICKAXE,
-	KNIFE,
-	STICK,
-	NO_TOOL,
+enum class Tool : int {
+    JUICER,
+    PICKAXE,
+    KNIFE,
+    STICK,
+    NO_TOOL,
 };
 
-std::ostream& operator<< (std::ostream& out, const Tool& t);
-std::istream& operator>> (std::istream& in, Tool& t);
-
-enum class TileType: int {
-	EMPTY,
-	STONE,
-	IRON,
-	TREE,
-	TURBINE,
-	WALL,
-	UNKNOWN,
+enum class TileType : int {
+    EMPTY,
+    STONE,
+    IRON,
+    TREE,
+    TURBINE,
+    WALL,
+    UNKNOWN,
 };
-
-std::ostream& operator<< (std::ostream& out, const TileType& t);
-std::istream& operator>> (std::istream& in, TileType& t);
 
 struct Tile {
-	TileType type;
-	
-	friend std::ostream& operator<< (std::ostream& out, const Tile& t) {
-		out << t.type;
-		return out;
-	}
+    TileType type;
+
+    friend std::ostream &operator<<(std::ostream &out, const Tile &t);
 };
 
-struct TurbineTile: Tile {
-	int lemon;
-	TurbineTile(int lemon): Tile(TileType::TURBINE), lemon(lemon) {};
-	
-	friend std::ostream& operator<< (std::ostream& out, const TurbineTile& t) {
-		out << t << " " << t.lemon;
-		return out;
-	}
+struct TurbineTile : Tile {
+    int lemon;
+
+    explicit TurbineTile(int lemon) : Tile{TileType::TURBINE}, lemon(lemon) {};
+
+    friend std::ostream &operator<<(std::ostream &out, const TurbineTile &t);
 };
 
-struct TreeTile: Tile {
-	bool has_lemon;
-	TreeTile(bool lemon): Tile(TileType::TREE), has_lemon(lemon) {};
-	
-	friend std::ostream& operator<< (std::ostream& out, const TreeTile& t) {
-		out << t << " " << t.has_lemon;
-		return out;
-	}
+struct TreeTile : Tile {
+    bool has_lemon;
+
+    explicit TreeTile(bool lemon) : Tile{TileType::TREE}, has_lemon(lemon) {};
+
+    friend std::ostream &operator<<(std::ostream &out, const TreeTile &t);
 };
 
 struct Lemur {
-	bool alive = true, is_stunned = false;
-	int x = 0, y = 0, iron = 0, lemon = 0, stone = 0;
-	std::vector<Tool> tools;
-	
-	friend std::istream& operator>> (std::istream& in, Lemur& l){
-		in >> l.alive;
-		if (l.alive) {
-			in >> l.x >> l.y >> l.iron >> l.lemon >> l.stone >> l.is_stunned;
-			std::string line;
-			std::getline(in, line);
-			std::istringstream line_in{line};
-			unsigned int i = 0;
-			for (Tool t; line_in >> t; i++) {
-				if (i < l.tools.size()) {
-					l.tools[i] = t;
-				} else {
-					l.tools.push_back(t);
-				}
-			}
-			if (i < l.tools.size()) l.tools.resize(i);
-		}
-		return in;
-	}
-	
-	friend std::ostream& operator<< (std::ostream& out, const Lemur& l) {
-		out << l.alive << " x: " << l.x << ", y: " << l.y << ", iron: " << l.iron;
-		out << ", lemon: " << l.lemon << ", stone: " << l.stone;
-		out << ", stunned: " << l.is_stunned <<", tools: " << l.tools;
-		return out;
-	}
+    bool alive = true, is_stunned = false;
+    int x = 0, y = 0, iron = 0, lemon = 0, stone = 0;
+    std::vector<Tool> tools;
+
+    friend std::istream &operator>>(std::istream &in, Lemur &l);
+
+    friend std::ostream &operator<<(std::ostream &out, const Lemur &l);
 };
 
 struct Player {
-	int id;
-	std::vector<Lemur> lemurs;
-	bool alive;
-	
-	Player () {
-		id = _id++;
-	}
-	
-	friend std::istream& operator>> (std::istream& in, Player& p){
-		int lemurs_count;
-		in >> lemurs_count;
-		p.lemurs.resize(lemurs_count);
-		
-		p.alive = false;
-		
-		for (int i = 0; i < lemurs_count; i++) {
-			in >> p.lemurs[i];
-			if (p.lemurs[i].alive) p.alive = true;
-		}
-		
-		return in;
-	}
-	
-	friend std::ostream& operator<< (std::ostream& out, const Player& p) {
-		if (p.alive) out << "alive";
-		else out << "dead ";
-		out << " player" << p.id << " lemurs: " << p.lemurs;
-		return out;
-	}
-	
-	private:
-		static int _id;
-};
+    int id;
+    std::vector<Lemur> lemurs;
+    bool alive = false;
 
-std::ostream& operator<< (std::ostream& out, const std::vector<Player>& v);
+    Player() {
+        id = _id++;
+    }
+
+    friend std::istream &operator>>(std::istream &in, Player &p);
+
+    friend std::ostream &operator<<(std::ostream &out, const Player &p);
+
+private:
+    static int _id;
+};
 
 
 struct World {
-	int width = 0;
-	int height = 0;
-	int my_id = 0;
-	std::vector<std::vector<Tile>> tiles;
-	std::vector<Player> players;
-	std::vector<std::vector<int>> oxygen;
+    int width = 0;
+    int height = 0;
+    int my_id = 0;
+    std::vector<std::vector<Tile>> tiles;
+    std::vector<Player> players;
+    std::vector<std::vector<int>> oxygen;
 
-	friend std::istream& operator>> (std::istream& in, World& w){
-		in >> w.width >> w.height;
-		
-		w.tiles.resize(w.height, std::vector<Tile>(w.width, {TileType::UNKNOWN}));
-		
-		for (int i = 0; i < w.height; i++) {
-			for (int j = 0; j < w.width; j++) {
-				TileType t;
-				in >> t;
-				switch (t) {
-					case TileType::TURBINE:
-						int lemon;
-						in >> lemon;
-						w.tiles[i][j] = TurbineTile{lemon};
-						break;
-					case TileType::TREE:
-						bool has_lemon;
-						in >> has_lemon;
-						w.tiles[i][j] = TreeTile{has_lemon};
-						break;
-					default:
-						w.tiles[i][j] = {t};
-				}
-			}
-		}
-		
-		int player_count;
-		in >> player_count >> w.my_id;
-		
-		w.players.resize(player_count);
-		
-		for (int i = 0; i < player_count; i++) in >> w.players[i];
-		
-		w.oxygen.resize(w.height, std::vector<int>(w.width, 0));
-		
-		for (int i = 0; i < w.height; i++)
-			for (int j = 0; j < w.width; j++)
-				in >> w.oxygen[i][j];
-		
-		char dot;
-		std::cin >> dot;
-		return in;
-	}
-	
-	friend std::ostream& operator<< (std::ostream& out, const World& w) {
-		out << w.width << " " << w.height << '\n';
-		out << w.tiles << w.players << w.oxygen;
-		return out;
-	}
+    friend std::istream &operator>>(std::istream &in, World &w);
+
+    friend std::ostream &operator<<(std::ostream &out, const World &w);
 };
 
 struct Command {
-	CommandType type;
+    CommandType type;
 };
 
-struct NOOP: Command {
-	NOOP(): Command(CommandType::NOOP) {};
+struct NOOP : Command {
+    NOOP() : Command{CommandType::NOOP} {};
 };
 
-struct STAB: Command {
-	int x, y;
-	STAB(int x, int y): Command(CommandType::STAB), x(x), y(y) {};
+struct STAB : Command {
+    int x, y;
+
+    STAB(int x, int y) : Command{CommandType::STAB}, x(x), y(y) {};
 };
 
-struct BONK: Command {
-	int x, y;
-	BONK(int x, int y): Command(CommandType::BONK), x(x), y(y) {};
+struct BONK : Command {
+    int x, y;
+
+    BONK(int x, int y) : Command{CommandType::BONK}, x(x), y(y) {};
 };
 
-struct BUILD: Command {
-	int x, y;
-	TileType tile;
-	BUILD(int x, int y, TileType tile):
-		Command(CommandType::BUILD),
-		x(x),
-		y(y),
-		tile(tile) {};
+struct BUILD : Command {
+    int x, y;
+    TileType tile;
+
+    BUILD(int x, int y, TileType tile) :
+            Command{CommandType::BUILD},
+            x(x),
+            y(y),
+            tile(tile) {};
 };
 
-struct BREAK: Command {
-	int x, y;
-	BREAK(int x, int y): Command(CommandType::BREAK), x(x), y(y) {};
+struct BREAK : Command {
+    int x, y;
+
+    BREAK(int x, int y) : Command{CommandType::BREAK}, x(x), y(y) {};
 };
 
-struct DISCARD: Command {
-	InventorySlot item;
-	int quantity;
-	DISCARD(InventorySlot item, int quantity):
-		Command(CommandType::DISCARD),
-		item(item),
-		quantity(quantity) {};
+struct DISCARD : Command {
+    InventorySlot item;
+    int quantity;
+
+    DISCARD(InventorySlot item, int quantity) :
+            Command{CommandType::DISCARD},
+            item(item),
+            quantity(quantity) {};
 };
 
-struct PUT: Command {
-	int x, y;
-	InventorySlot item;
-	int quantity;
-	PUT(int x, int y, InventorySlot item, int quantity):
-		Command(CommandType::PUT),
-		x(x),
-		y(y),
-		item(item),
-		quantity(quantity) {};
+struct PUT : Command {
+    int x, y;
+    InventorySlot item;
+    int quantity;
+
+    PUT(int x, int y, InventorySlot item, int quantity) :
+            Command{CommandType::PUT},
+            x(x),
+            y(y),
+            item(item),
+            quantity(quantity) {};
 };
 
-struct TAKE: Command {
-	int x, y;
-	InventorySlot item;
-	int quantity;
-	TAKE(int x, int y, InventorySlot item, int quantity):
-		Command(CommandType::TAKE),
-		x(x),
-		y(y),
-		item(item),
-		quantity(quantity) {};
+struct TAKE : Command {
+    int x, y;
+    InventorySlot item;
+    int quantity;
+
+    TAKE(int x, int y, InventorySlot item, int quantity) :
+            Command{CommandType::TAKE},
+            x(x),
+            y(y),
+            item(item),
+            quantity(quantity) {};
 };
 
-struct CRAFT: Command {
-	Tool tool;
-	CRAFT(Tool tool): Command(CommandType::CRAFT), tool(tool) {};
+struct CRAFT : Command {
+    Tool tool;
+
+    explicit CRAFT(Tool tool) : Command{CommandType::CRAFT}, tool(tool) {};
 };
 
-struct MOVE: Command {
-	int x, y;
-	MOVE(int x, int y): Command(CommandType::MOVE), x(x), y(y) {};
+struct MOVE : Command {
+    int x, y;
+
+    MOVE(int x, int y) : Command{CommandType::MOVE}, x(x), y(y) {};
 };
 
+/**
+* Print any vector to output
+* @tparam T type inside vector
+* @param out stream to which output will we written
+* @param v input vector
+* @return out stream
+*/
+namespace std {
+    template<class T>
+    ostream &operator<<(ostream &out, const vector<T> &v) {
+        ranges::copy(v, ostream_iterator<T>(out, " "));
+        out << '\n';
+        return out;
+    }
+}
 
-void greet_server (const char *name, const char *color);
+std::ostream &operator<<(std::ostream &out, const CommandType &cmd);
 
-void send_commands (const std::vector<Command>& cmd);
+std::ostream &operator<<(std::ostream &out, const InventorySlot &i);
 
-void end_communication ();
+std::ostream &operator<<(std::ostream &out, const Tool &t);
 
+std::istream &operator>>(std::istream &in, Tool &t);
+
+std::ostream &operator<<(std::ostream &out, const TileType &t);
+
+std::istream &operator>>(std::istream &in, TileType &t);
+
+
+void greet_server(const char *name, const char *color);
+
+void send_commands(const std::vector<Command> &cmd);
 
 #endif
--- templates/cpp/main.cpp
+++ templates/cpp/main.cpp
@@ -4,32 +4,32 @@ using namespace std;
 
 #define NAME "cpp"
 #define COLOR "00ff00"
- 
-int  DX[] = { 0, 1, 0, -1};
-int  DY[] = {-1, 0, 1,  0};
+
+int DX[] = {0, 1, 0, -1};
+int DY[] = {-1, 0, 1, 0};
 
 World world;
 
 // sem pis svoj kod
 vector<Command> do_turn() {
-	Player& myself = world.players[world.my_id];
-	vector<Command> commands(myself.lemurs.size(), NOOP());
-	cerr << world.players;
-	cerr << myself;
-	
-	return commands;
+    Player &myself = world.players[world.my_id];
+    vector<Command> commands(myself.lemurs.size(), NOOP());
+    cerr << world.players;
+    cerr << myself;
+
+    return commands;
 }
 
 int main() {
-	// aby sme mali nahodu
-	srand(time(0));
-	// povieme serveru ako sa chceme volat a farbu
-	greet_server(NAME, COLOR);
-	// robime tahy kym sme zivy
-	do {
-		cin >> world;
-		send_commands(do_turn());
-	} while (world.players[world.my_id].alive);
-	cout << "Bye\n";
+    // aby sme mali nahodu
+    srand(time(nullptr));
+    // povieme serveru ako sa chceme volat a farbu
+    greet_server(NAME, COLOR);
+    // robime tahy kym sme zivy
+    do {
+        cin >> world;
+        send_commands(do_turn());
+    } while (world.players[world.my_id].alive);
+    cout << "Bye\n";
 }
 
